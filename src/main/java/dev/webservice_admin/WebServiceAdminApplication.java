b4f474076916227283f901126529e5713ece27f4
MSI\Minh Tan <tanlun04112000@gmail.com>
2022-02-24 11:14:18 +0700

    Bắt đầu với trang web quản lý

---
+++ src/main/java/dev/webservice_admin/WebServiceAdminApplication.java
@@ -0,0 +1,13 @@
+package dev.webservice_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceAdminApplication.class, args);
+    }
+
+}
