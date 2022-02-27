8b25ce2cb297131a52e5779a28d3692e3a2ec039
MSI\Minh Tan <tanlun04112000@gmail.com>
2022-02-27 20:32:27 +0700

    sửa lại vị trí trên datatables

--- src/main/java/dev/webservice_admin/controller/ProductController.java
+++ src/main/java/dev/webservice_admin/controller/ProductController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class HomeController {
+@RequestMapping("/product")
+public class ProductController {
 
     @GetMapping
     public String home() {
-        return "index";
+        return "product";
     }
 
 }
