f6a99de510ecf659b068a1b5abda264d29cf3170
MSI\Minh Tan <tanlun04112000@gmail.com>
2022-03-10 22:11:10 +0700

    đổi tên đăng nhập

--- src/main/java/dev/webservice_admin/controller/OrderController.java
+++ src/main/java/dev/webservice_admin/controller/OrderController.java
@@ -2,28 +2,23 @@ package dev.webservice_admin.controller;
 
 import dev.webservice_admin.model.ChiTietDonDatHang;
 import dev.webservice_admin.model.DonDatHang;
-import dev.webservice_admin.model.NhanVien;
+
 import dev.webservice_admin.service.ChiTietDonDatHangService;
 import dev.webservice_admin.service.DonDatHangService;
-import dev.webservice_admin.service.NhanVienService;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.security.Principal;
 import java.util.List;
 
 @Controller
 @RequestMapping("/order")
 public class OrderController {
-
-    @Autowired
-    private NhanVienService nhanVienService;
 
     @Autowired
     private DonDatHangService donDatHangService;
@@ -32,12 +27,7 @@ public class OrderController {
     private ChiTietDonDatHangService chiTietDonDatHangService;
 
     @GetMapping
-    public String orderPage(Model model, Principal principal) {
-
-        User loginedUser = (User) ((Authentication) principal).getPrincipal();
-        NhanVien nhanVien = nhanVienService.findByUsername(loginedUser.getUsername());
-        model.addAttribute("nhanVien", nhanVien);
-
+    public String orderPage() {
         return "order";
     }
 
