1dda958dd95e48d134b4f1b44b19a8cdcc941da7
MSI\Minh Tan <tanlun04112000@gmail.com>
2022-03-10 22:11:40 +0700

    đổi tên đăng nhập

--- src/main/java/dev/webservice_admin/controller/ProductController.java
+++ src/main/java/dev/webservice_admin/controller/ProductController.java
@@ -1,8 +1,5 @@
 package dev.webservice_admin.controller;
 
-import dev.webservice_admin.service.NhanVienService;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/dev/webservice_admin/controller/ProductTypeController.java
+++ src/main/java/dev/webservice_admin/controller/ProductTypeController.java
@@ -1,14 +1,9 @@
 package dev.webservice_admin.controller;
 
-import dev.webservice_admin.service.NhanVienService;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
 
 @Controller
 @RequestMapping("/type-product")
