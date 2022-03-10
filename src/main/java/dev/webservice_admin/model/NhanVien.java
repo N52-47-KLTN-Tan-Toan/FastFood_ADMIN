f11f83009fbf8e714209f78c9aa5b45977b5053b
MSI\Minh Tan <tanlun04112000@gmail.com>
2022-03-10 16:22:18 +0700

    làm đăng nhập

--- src/main/java/dev/webservice_admin/model/NhanVien.java
+++ src/main/java/dev/webservice_admin/model/NhanVien.java
@@ -1,10 +1,8 @@
 package dev.webservice_admin.model;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Builder
 @Getter
 @NoArgsConstructor
 public class NhanVien extends User{
